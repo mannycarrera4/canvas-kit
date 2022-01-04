7f78cd4fd97b1d76485c2e626a2906b822e3777a
manuel carrera <manuel.carrera@workday.com>
2022-01-04 14:33:12 -0700

    chore: Upgrade to react 17

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   env: {
     test: {
-      presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-typescript',
+        '@babel/preset-react',
+        {runtime: 'automatic'},
+      ],
       plugins: ['emotion', '@babel/proposal-class-properties', '@babel/plugin-transform-runtime'],
     },
   },
