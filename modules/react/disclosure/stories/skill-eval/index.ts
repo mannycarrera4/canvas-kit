9dff9d33216e876c12af293df54a6a0929cc4c5d
manuel.carrera <manuel.carrera@workday.com>
2026-09-11 08:56:38 -0600

    fix: Remove skill-eval

--- modules/react/disclosure/stories/skill-eval/index.ts
+++
@@ -1,9 +0,0 @@
-export {Disclosure} from './Disclosure';
-export type {DisclosureProps} from './Disclosure';
-export {DisclosureTarget} from './DisclosureTarget';
-export type {DisclosureTargetProps} from './DisclosureTarget';
-export {DisclosureContent} from './DisclosureContent';
-export type {DisclosureContentProps} from './DisclosureContent';
-export {useDisclosureModel} from './useDisclosureModel';
-export {useDisclosureTarget} from './useDisclosureTarget';
-export {useDisclosureContent} from './useDisclosureContent';
--- modules/react/disclosure/stories/skill-eval/useDisclosureContent.ts
+++
@@ -1,8 +0,0 @@
-import {createElemPropsHook} from '@workday/canvas-kit-react/common';
-
-import {useDisclosureModel} from './useDisclosureModel';
-
-export const useDisclosureContent = createElemPropsHook(useDisclosureModel)(model => ({
-  id: model.state.id,
-  hidden: !model.state.visible,
-}));
