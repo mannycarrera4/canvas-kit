1b654668b076357f9f904eb8e140bceba9541fe7
manuel.carrera <manuel.carrera@workday.com>
2026-09-11 09:01:28 -0600

    fix: Remove eval

--- modules/react/collection/stories/skill-eval/index.ts
+++
@@ -1,6 +0,0 @@
-export {TagList} from './TagList';
-export type {TagListProps, TagListItemValue} from './TagList';
-export {TagListItem} from './TagListItem';
-export type {TagListItemProps} from './TagListItem';
-export {useTagListItem} from './useTagListItem';
-export {useTagListModel} from './useTagListModel';
